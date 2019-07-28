9615ad2810247a63c58a3ea6126427a39369b5d5
Ninja <ninaoverkott@gmail.com>
2019-07-28 13:10:17 +0200

    Cleaned home.php

--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -3,9 +3,6 @@
 <main>
 
 
-    <div class="timeline">Don't you worry, it's still a test</div>
-    <div id="JohannesBKerning">Basilikum ist gut!</div>
-    <div class="timeline">Is it still a test? Is EVERYTHING a test?</div>
 
 
 </main>
