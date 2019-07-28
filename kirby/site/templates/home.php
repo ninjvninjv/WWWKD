5665b06cc5b243e9e4f6761b36bd228f254b4029
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-07-28 13:38:01 +0200

    Janosch im Code

--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -2,10 +2,13 @@
 
 <main>
 
+    <?php $janosch = $site->find('people')->find('janoschbelakratz')?>
+    
 <div><?= $site->find("people")->find("jonadienst")->Prename() ?> was here.</div>
 
   <p><?= $site->find('people')->find('ninaoverkott')->prename() ?> <?= $site->find('people')->find('ninaoverkott')->name() ?></p>
 
+    <div id="student <?= $janosch->Title() ?>"> Mein Name ist <?= $janosch->Prename() ?> <?= $janosch->Name() ?></div>
 
 </main>
 
