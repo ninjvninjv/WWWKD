510f7960223314d7366bc390d1e25486e885486f
coldchicken2 <jmr.schaefer@gmail.com>
2019-07-28 13:48:51 +0200

    brought home-oho up to newly agreed writing conventions

--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -8,16 +8,16 @@
 <?php $janosch = $site->find('people')->find('janoschbelakratz')?>
     
 
-<div id="Johanna" style="font-family: serif; color: blue;">
+<p style= 'font-family: serif; color: blue;'>
 
-    <?= $site->find("people")->find("johannaschaefer")->Prename()?> <?= $site->find("people")->find("johannaschaefer")->Name()?>
-</div>
+    <?= $site->find('people')->find('johannaschaefer')->Prename()?> <?= $site->find('people')->find('johannaschaefer')->Name()?>
+</p>
     
-<div><?= $site->find("people")->find("jonadienst")->Prename() ?> was here.</div>
+<div><?= $site->find('people')->find('jonadienst')->Prename() ?> was here.</div>
 
   <p><?= $site->find('people')->find('ninaoverkott')->prename() ?> <?= $site->find('people')->find('ninaoverkott')->name() ?></p>
 
-    <div id="student <?= $janosch->Title() ?>"> Mein Name ist <?= $janosch->Prename() ?> <?= $janosch->Name() ?></div>
+    <p id='student <?= $janosch->Title() ?>'> Mein Name ist <?= $janosch->Prename() ?> <?= $janosch->Name() ?></p>
 
 </main>
 
