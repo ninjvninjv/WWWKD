20670ef1d316c1297f418bdeb9d410994de60358
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-07-28 11:47:42 +0200

    habs mal gelöscht

--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -6,11 +6,9 @@
     <div class="timeline">Don't you worry, it's still a test</div>
     <div id="JohannesBKerning">Basilikum ist gut!</div>
     <div class="timeline">Is it still a test? Is EVERYTHING a test?</div>
-<<<<<<< HEAD
     <div class="timeline">Jona is a test bot.</div>
     <div class="text">Will it break?</div>
-=======
->>>>>>> parent of ddb53de... Update home.php
+
 
 </main>
 
