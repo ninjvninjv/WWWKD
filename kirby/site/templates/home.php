54a26e9af2d855fa72b80d7b54a6ef02642cd7c5
coldchicken2 <jmr.schaefer@gmail.com>
2019-07-28 13:33:16 +0200

    wrote Johanna on home-php

--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -1,7 +1,11 @@
 <?php snippet('header') ?>
 
 <main>
-
+<div id="Johanna" style="font-family: serif;">
+    <?= $site->find("people")->find("johannaschaefer")->Prename()?> <?= $site->find("people")->find("johannaschaefer")->Name()?>
+</div>
+    
+    
 
 
 
