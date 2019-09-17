22ac495efe9aba4c7d1ada378b5b4a72b220d244
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-09-17 17:51:35 +0200

    Calendar dates

--- kirby/index.php
+++ kirby/index.php
@@ -2,4 +2,6 @@
 
 require __DIR__ . '/kirby/bootstrap.php';
 
+
 echo (new Kirby)->render();
+
--- kirby/site/snippets/timeline.php
+++ kirby/site/snippets/timeline.php
@@ -5,17 +5,21 @@
     <p>(timeline)</p>
     
     <?php 
-         $period = new DatePeriod(
-             new DateTime('2019-09-01'),
-             new DateInterval('P1D'),
-             new DateTime('2019-12-30')
+    
+        $begin = new DateTime('2019-09-01');
+        $interval = new DateInterval('P1D');
+        $end = new DateTime('2019-12-30');
+        $period = new DatePeriod(
+             $begin,
+             $interval,
+             $end
          ); 
     
     foreach ($period as $key => $value):
     
     ?>
     
-    <div class="day"><?= $key ?></div>
+    <div class="day" id="tl<?= $value->format("Ymd"); ?>"><?= $value->format("Y.m.d"); ?></div>
     
     <?php endforeach?>
     
