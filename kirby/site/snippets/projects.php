b5f969c05bf89d4ec1513fa3af8fb489a89fa42c
coldchicken2 <jmr.schaefer@gmail.com>
2019-09-17 17:49:47 +0200

    projectsbilder

--- kirby/site/snippets/projects.php
+++ kirby/site/snippets/projects.php
@@ -7,17 +7,29 @@
 
         <?php if($semester->children()->isNotEmpty()): ?>
             <br>
-            <p><?= $semester->title() ?></p> 
+            <div class="prosemester"><?= $semester->title() ?></div> 
             
     
             <?php foreach($semester->children() as $project): ?>
-    
                 <p><?= $project->title() ?></p>
-    
+                <?php foreach($project->images() as $image): ?>
+                    <?php if ($image->template()=="cover"): ?>
+                        <img id="proimage" src="<?= $image->url() ?>">
+                    <?php endif ?>
+                <?php endforeach ?> 
             <?php endforeach ?> 
+    
+           
     
         <?php endif ?> 
 
     <?php endforeach ?>
     
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+
+  /* On click, display projectinfo */
+
+</script>
