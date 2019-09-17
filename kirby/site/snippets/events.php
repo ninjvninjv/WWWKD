a38f2d8bf05abf957ed3051761db908045a286d8
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-09-17 13:42:31 +0200

    calendar date

--- kirby/site/snippets/events.php
+++ kirby/site/snippets/events.php
@@ -8,7 +8,7 @@
          $period = new DatePeriod(
              new DateTime('2019-09-01'),
              new DateInterval('P1D'),
-             new DateTime('2019-09-30')
+             new DateTime('2019-12-30')
          ); 
     
     foreach ($period as $key => $value):
