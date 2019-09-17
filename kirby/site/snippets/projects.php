927e7883b4ea666f6551636421301b59bbe830e3
coldchicken2 <jmr.schaefer@gmail.com>
2019-09-17 14:34:20 +0200

    projects snippet

--- kirby/site/snippets/projects.php
+++ kirby/site/snippets/projects.php
@@ -1,16 +1,17 @@
 <div class="col" id="projectcol">
     
     <p>(projects)</p>
-    <?php for ($i=10; $i<=30; $i++):?>
-    <p> SS 20<?= $i?> </p>  
-    <p> WS 20<?= $i?> / 20<?= $i+1 ?> </p>
+    
+    
+    <?php foreach($site->find('projects')->children() as $semester): ?>
 
-        <?php foreach($site->find('projects')->children() as $project): $t = $project->semester(); ?>
-            <?php if ($t == "WS$i"):?>
-            <p> <?= $project->title()?> </p>
-            <?php endif ?>
+        <?php if ($semester->isEmpty()): ?>
+            <p><?= $semester->title() ?></p>   
+            <?php foreach ($semester->children() as $project): ?>
+                <p><?php $project->title() ?></p>
+            <?php endforeach ?>
+        <?php endif ?> 
 
-        <?php endforeach ?>
-    <?php endfor ?>
+    <?php endforeach ?>
     
 </div>
