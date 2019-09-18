a90cad950e1f4e6696332f98ad28b0ab690f4754
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-09-18 16:22:24 +0200

    added google Calendar API

--- kirby/site/snippets/seminars.php
+++ kirby/site/snippets/seminars.php
@@ -4,23 +4,39 @@
     
     <p>(seminars)</p>
     
-    <?php 
+    <?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/calendar/v3/calendars/4e76jq9kp5tooil0nbl92uvn68@group.calendar.google.com/events?key=AIzaSyAun17rUsshrEgSRoG8c0dxlRmkgkBu-78&timeMin=2010-01-01T10:00:00-00:00&orderBy=startTime&singleEvents=true&maxResults=2500');
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+
+$json_response = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+if ($status != 200) {
+    die("Error: $status, response $json_response, curl_error " . curl_error($curl) . ', curl_errno ' . curl_errno($curl));
+}
+curl_close($curl);
+
+
+$json_array = json_decode($json_response);
+
+$items = $json_array->items;
+
+foreach($items as $item):
+?>
+    <div class="day">
+        <div class="course" id="">
     
-        $begin = new DateTime('2019-09-01');
-        $interval = new DateInterval('P1D');
-        $end = new DateTime('2019-12-30');
-        $period = new DatePeriod(
-             $begin,
-             $interval,
-             $end
-         ); 
+            <div class="seminarteacher"><?= $item->summary; ?></div>
+            <div class="seminarstart"><?= $item->start->dateTime; ?></div>
+            <div class="seminarend"><?= $item->end->dateTime; ?></div>
     
-    foreach ($period as $key => $value):
+        </div>
+    </div>
     
-    ?>
-    
-    <div class="day" id="s<?= $value->format("Ymd"); ?>"></div>
-    
-    <?php endforeach?>
+<?php endforeach; $filename = fopen("result.json", "w") or die("Unable to open file!");
+fwrite($filename, $json_response);
+fclose($filename);?>
     
 </div>
