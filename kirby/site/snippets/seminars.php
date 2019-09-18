9b871b315fb60f14506a08405ae91aef70fdfb2b
Johannes B Kerning <Janosch.bela.kratz@gmx.de>
2019-09-18 17:53:34 +0200

    calendar basics api calling
    still doesnt work

--- kirby/site/snippets/seminars.php
+++ kirby/site/snippets/seminars.php
@@ -4,28 +4,78 @@
     
     <p>(seminars)</p>
     
-    <?php
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/calendar/v3/calendars/4e76jq9kp5tooil0nbl92uvn68@group.calendar.google.com/events?key=AIzaSyAun17rUsshrEgSRoG8c0dxlRmkgkBu-78&timeMin=2010-01-01T10:00:00-00:00&orderBy=startTime&singleEvents=true&maxResults=2500');
+        <?php 
+    
+        $day = array();
+        $i = 0;
+        $begin = new DateTime('2019-01-01');
+        $interval = new DateInterval('P1D');
+        $end = new DateTime('2019-12-30');
+        $period = new DatePeriod(
+             $begin,
+             $interval,
+             $end
+         ); 
+    
+        foreach ($period as $key => $value):
+    
+        $day[$i] = $value;
+        $i++;
+    
+    ?>
+    
+        <div class="day" id="<?= $value->format("Y-m-d"); ?>"></div>
+    
+    
+    <?php endforeach?>
+    
 
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+            <?php
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/calendar/v3/calendars/4e76jq9kp5tooil0nbl92uvn68@group.calendar.google.com/events?key=AIzaSyAun17rUsshrEgSRoG8c0dxlRmkgkBu-78&timeMin=2010-01-01T10:00:00-00:00&orderBy=startTime&singleEvents=true&maxResults=2500');
 
-$json_response = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-if ($status != 200) {
-    die("Error: $status, response $json_response, curl_error " . curl_error($curl) . ', curl_errno ' . curl_errno($curl));
-}
-curl_close($curl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+
+        $json_response = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($status != 200) {
+            die("Error: $status, response $json_response, curl_error " . curl_error($curl) . ', curl_errno ' . curl_errno($curl));
+        }
+        curl_close($curl);
 
 
-$json_array = json_decode($json_response);
+        $json_array = json_decode($json_response);
 
-$items = $json_array->items;
+        $items = $json_array->items;
 
-foreach($items as $item):
-?>
-    <div class="day">
+        foreach($items as $item):
+        ?>
+
+        <div class="day"><?= $item->summary; ?></div>
+                   <script type='text/javascript'>
+
+                $(document).ready(function(){
+                    
+                    var $timestamp = <?= $item->start->dateTime; ?>;
+                    var seminardate = $timestamp.substring(0, 10);
+
+                        $('<?= $item->start->dateTime; ?>').appendTo('#'+seminardate);
+                    
+                });
+
+            </script>
+    
+        <?php endforeach; $filename = fopen("result.json", "w") or die("Unable to open file!");
+        fwrite($filename, $json_response);
+        fclose($filename);?>
+
+    
+</div>
+
+<!--html structure seminar-->
+<!--
+<div class="day">
         <div class="course" id="">
     
             <div class="seminarteacher"><?= $item->summary; ?></div>
@@ -33,10 +83,4 @@ foreach($items as $item):
             <div class="seminarend"><?= $item->end->dateTime; ?></div>
     
         </div>
-    </div>
-    
-<?php endforeach; $filename = fopen("result.json", "w") or die("Unable to open file!");
-fwrite($filename, $json_response);
-fclose($filename);?>
-    
-</div>
+</div>-->
