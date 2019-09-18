2d08052ea6554c32541f0157d2cdf8bc0f236387
coldchicken2 <jmr.schaefer@gmail.com>
2019-09-18 17:52:58 +0200

    sr

--- kirby/site/snippets/people.php
+++ kirby/site/snippets/people.php
@@ -1,5 +1,18 @@
 <div class="col" id="peoplecol">
     
-    <p>(people)</p>
+    <p class="orientation">(people)</p>
+    <div class="colcontent">
+        <?php foreach ($site->find ('people')->children() as $person): ?>
+        <div class="person">
+            <br>
+            <br>
+            <?= $person->title() ?>
+            <br>
+            <br>
+            <?= $person->about() ?>
+        </div>
+
+        <?php endforeach ?>
+    </div> 
     
 </div>
