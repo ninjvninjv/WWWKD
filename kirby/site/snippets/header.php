598553fe5484349af74e6f78898d75047621f9fc
Ninja <ninaoverkott@gmail.com>
2019-12-03 15:51:41 +0100

    Litte Navigation Style Fix

--- kirby/site/snippets/header.php
+++ kirby/site/snippets/header.php
@@ -11,7 +11,6 @@
     <?= css(['/assets/css/index.css', '@auto']) ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="/assets/js/jquery-3.1.0.min.js"></script>
 
 </head>
 <body>
