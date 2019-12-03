c890c6cc6737b4d21b3e89262034ff2a580e0296
Ninja <ninaoverkott@gmail.com>
2019-12-03 14:16:52 +0100

    Column nav v.01
    Still unfinished!
    - no dropmenues yet
    - no scrolling yet

--- kirby/site/snippets/header.php
+++ kirby/site/snippets/header.php
@@ -9,9 +9,9 @@
     <?= css(['/assets/css/templates/fonts.css', '@auto']) ?>
     <?= css(['/assets/css/templates/home.css', '@auto']) ?>
     <?= css(['/assets/css/index.css', '@auto']) ?>
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="/assets/js/jquery-3.1.0.min.js"></script>
 
 </head>
 <body>
--- kirby/site/snippets/projects.php
+++ kirby/site/snippets/projects.php
@@ -1,26 +1,25 @@
 <div class="col" id="projectcol">
-    
-    <p class="orientation">(projects)</p>
+
     <div class="colcontent">
             <?php foreach($site->find('projects')->children() as $semester): ?>
 
                 <?php if($semester->children()->isNotEmpty()): ?>
-                    <div class="prosemester"><?= $semester->title() ?></div> 
+                    <div class="prosemester"><?= $semester->title() ?></div>
 
 
                     <?php foreach($semester->children() as $project): ?>
 
                         <div class="project">
-                            
+
                             <div class="seminarGlow" id="projectGlowDiplom"></div>
-                            
+
                             <div class="protitle"><?= $project->title() ?> <br>(<?= $project->type() ?>)</div>
 
                             <?php foreach($project->images() as $image): ?>
                                 <?php if ($image->template()=="cover"): ?>
                                     <img class="proimage" src="<?= $image->url() ?>">
                                 <?php endif ?>
-                            <?php endforeach ?> 
+                            <?php endforeach ?>
 
 
 
@@ -28,32 +27,32 @@
                             <!-- goes into the persons textfile and takes data -->
                             <div class="proauthor">
 
-                                <?php $i =0; foreach ($project->author()->toStructure() as $author): 
+                                <?php $i =0; foreach ($project->author()->toStructure() as $author):
 
                                     $autorpath = $author->path();
                                     $i++;
 
                                 ?>
-                                
+
                                 <?php if($i > 1): ?>
-                                
+
                                 &amp; <?= $autorpath->pages()->toPage()->title() ?>
-                                
+
                                 <?php else: ?>
-                                
+
                                 von <?= $autorpath->pages()->toPage()->title() ?>
-                                
+
                                 <?php endif ?>
 
                                 <?php endforeach ?>
 
                             </div>
-                            
-                        </div> 
 
-                    <?php endforeach ?> 
+                        </div>
 
-                <?php endif ?> 
+                    <?php endforeach ?>
+
+                <?php endif ?>
 
             <?php endforeach ?>
     </div>
@@ -64,7 +63,7 @@
 <script type="text/javascript">
 
   /* On click, display projectinfo */
-    
-    
+
+
 
 </script>
